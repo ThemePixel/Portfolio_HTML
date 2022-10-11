9b66ec1b754e235136d326cd1e05c4ab46492274
z9110858@gmail.com <z9110858@gmail.com>
2022-10-12 04:34:58 +0700

    header inverted added

--- scripts.js
+++ scripts.js
@@ -89,35 +89,24 @@
 
 
 
-    /////////////////////SWIPER///////////////////////////////////////////////////
-    var swiper = new Swiper('.swiper-container', {
-        //initialSlide: 1,
-        slidesPerView: 9,
-        //spaceBetween: 20,
-      
+    
+    /////////////////////////////////////////header color////////////////////////////////////////////////////////////////////////////  
+        
+    var className = "inverted";
+    var scrollTrigger = 60;
+    
+    window.onscroll = function() { 
+      // We add pageYOffset for compatibility with IE.
+      if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+        document.getElementsByTagName("header")[0].classList.add(className);
+      } else {
+        document.getElementsByTagName("header")[0].classList.remove(className);
+      }
+    };
        
-      
-        centeredSlides: true,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      });
-      
-      
-      
-      
-          /* hide left arrow by deafult */
-          var arrow = document.getElementsByClassName('swiper-button-prev')[0];
-          arrow.style.display = 'none';
-          /* Swiper API - if index = 1 hide left arrow / otherwise show */
-          swiper.on('slideChange', function() {
-            var realIndex = swiper.realIndex;
-            if (realIndex == 0) {
-              console.log("real index:" + realIndex + " - hide arrow");
-              arrow.style.display = 'none';
-            } else {
-              console.log("real index:" + realIndex + " - show arrow");
-              arrow.style.display = 'block';
-            }
-        });
+   /////////////////////////////////////////header color////////////////////////////////////////////////////////////////////////////     
+        
+
+
+/////////////////////////////////////////////////////parallax//////////////////////////////////////////////////////////////////////////////////////
+       
