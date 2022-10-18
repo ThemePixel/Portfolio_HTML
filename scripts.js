97f68ea7c3021decb124fe912e07cc9892fe9488
z9110858@gmail.com <z9110858@gmail.com>
2022-10-18 14:48:03 +0700

    window.addEventListener('scroll', () => {     const currentScroll = window.pageYOffset

--- scripts.js
+++ scripts.js
@@ -108,5 +108,35 @@
         
 
 
-/////////////////////////////////////////////////////parallax//////////////////////////////////////////////////////////////////////////////////////
-       
+/////////////////////////////////////////////////////scroll up//////////////////////////////////////////////////////////////////////////////////////
+
+let body = document.body;
+let lastscroll = 0;
+
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset
+
+    if (currentScroll <= 0) {
+        body.classList.remove("scroll-up")
+    } 
+ 
+    if (currentScroll > lastscroll && !body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-up")
+        body.classList.add("scroll-down")
+    }
+
+    if (currentScroll < lastscroll && body.classList.contains("scroll-down")) {
+        body.classList.remove("scroll-down")
+        body.classList.add("scroll-up")
+    }
+    lastscroll = currentScroll;
+
+
+
+})
+
+
+
+
+/////////////////////////////////////////////////////scroll up//////////////////////////////////////////////////////////////////////////////////////
