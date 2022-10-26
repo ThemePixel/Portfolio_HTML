cd4eed00d738e86c20ff09e172c0dda080195470
z9110858@gmail.com <z9110858@gmail.com>
2022-10-26 19:15:58 +0700

    start html..// and ..//..//

--- scripts.js
+++ scripts.js
@@ -136,6 +136,21 @@ window.addEventListener('scroll', () => {
 
 })
 
+///////////////////////////////////////////////////
+var str = document.getElementsByTagName('code')[0].innerHTML.toString();
+var i = 0;
+document.getElementsByTagName('code')[0].innerHTML = "";
+
+setTimeout(function() {
+    var se = setInterval(function() {
+        i++;
+        document.getElementsByTagName('code')[0].innerHTML = str.slice(0, i) + "|";
+        if (i == str.length) {
+            clearInterval(se);
+            document.getElementsByTagName('code')[0].innerHTML = str;
+        }
+    }, 10);
+},0);
 
 
 
