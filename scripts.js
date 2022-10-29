58a0e80070792675159757c3ac7f34f796b9b56c
z9110858@gmail.com <z9110858@gmail.com>
2022-10-29 09:05:30 +0700

    main__container start

--- scripts.js
+++ scripts.js
@@ -155,3 +155,23 @@ setTimeout(function() {
 
 
 /////////////////////////////////////////////////////scroll up//////////////////////////////////////////////////////////////////////////////////////
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+        if (change.isIntersecting) {
+            change.target.classList.add('element-show');
+        } else {
+            change.target.classList.remove('element-show');
+        }
+    });
+} 
+ 
+let options = {
+    threshold: [0.2] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.reveal__div');
+
+for (let elm of elements) {
+    observer.observe(elm);
+}
