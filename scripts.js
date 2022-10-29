d11922879373673c3a99a4d1cc6fb18246d90381
z9110858@gmail.com <z9110858@gmail.com>
2022-10-29 18:47:06 +0700

    loading-lazy script

--- scripts.js
+++ scripts.js
@@ -175,3 +175,46 @@ let elements = document.querySelectorAll('.reveal__div');
 for (let elm of elements) {
     observer.observe(elm);
 }
+
+
+
+//////////////////////////////////////loading-lazy__section///////////////////////////////////////////////////////
+
+const lazyImages = document.querySelectorAll('img[data-src]');
+const windowHeight = document.documentElement.clientHeight;
+
+let lazyImagesPosition = [];
+if (lazyImages.length > 0) {
+    lazyImages.forEach(img => {
+        if (img.dataset.src || img.dataset.srcset) {
+            lazyImagesPosition.push(img.getBoundingClientRect().top + pageYOffset);
+            lazyScrollCheck();
+        }
+    });
+}
+ 
+window.addEventListener("scroll", lazyScroll);
+
+function lazyScroll() {
+    if(document.querySelectorAll('img[data-src]').length > 0) {
+        lazyScrollCheck();
+    }
+}
+
+function lazyScrollCheck() {
+    let imgIndex = lazyImagesPosition.findIndex(
+        item => pageYOffset > item - windowHeight
+        );
+        if (imgIndex >= 0) {
+            if (lazyImages[imgIndex].dataset.src) {
+                lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+                lazyImages[imgIndex].removeAttribute('data-src');
+            }
+            delete lazyImagesPosition[imgIndex];
+        }
+}
+
+
+
+
+//////////////////////////////////////loading-lazy__section///////////////////////////////////////////////////////
