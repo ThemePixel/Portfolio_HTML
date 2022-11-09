71f1eebcd4a00fd8730803154821c32280a2ee6b
z9110858@gmail.com <z9110858@gmail.com>
2022-11-10 01:18:19 +0700

    del code del glich added right background background: url(../img/image__one.jpg) no-repeat 50% -190px / cover;

--- scripts.js
+++ scripts.js
@@ -137,22 +137,6 @@ window.addEventListener('scroll', () => {
 
 
 
-///////////////////////////////////////////////////java text/////////////////////////////////////////////////
-var str = document.getElementsByTagName('code')[0].innerHTML.toString();
-var i = 0;
-document.getElementsByTagName('code')[0].innerHTML = "";
-
-setTimeout(function() {
-    var se = setInterval(function() {
-        i++;
-        document.getElementsByTagName('code')[0].innerHTML = str.slice(0, i) + "|";
-        if (i == str.length) {
-            clearInterval(se);
-            document.getElementsByTagName('code')[0].innerHTML = str;
-        }
-    }, 10);
-},0);
-///////////////////////////////////////////////////java text/////////////////////////////////////////////////
 
 
 
