1efeda493a42c9a9831b8753eaaa43e42d130acf
z9110858@gmail.com <z9110858@gmail.com>
2022-12-10 23:25:55 +0700

    tabs done v.1.0

--- scripts.js
+++ scripts.js
@@ -212,3 +212,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 //////////////////////////////////////3d slider with swiper slider///////////////////////////////////////////////////////
+const tabs = document.querySelectorAll('.tabs_section-ul .tabs_section-li');
+const divs = document.querySelectorAll('.tabs_section_buttom-content .tabs_section_buttom-box');
+
+tabs.forEach((tab) => {
+    //Add click for each li
+    tab.addEventListener("click", function (e) {
+        tabs.forEach((tab) => {
+            //On click remove class Active from all Li
+            tab.classList.remove("active");
+        });
+        //Add Class Active On the li 
+        e.currentTarget.classList.add("active");
+        divs.forEach((div) => {
+            //Display None for all content Divs on click
+            div.style.display = 'none';
+            //Select the div the contain the class of data-content and make it display flex
+            document.querySelector('.' + e.currentTarget.dataset.content).style.display = 'flex';
+        });
+    });
+})
